11be82e2357f4ac53c139ef5fd41b8e9ca5c72b3
Philip Knapp <philipsknapp@gmail.com>
2025-09-02 16:32:26 -0500

    updated selectAll route, added select route

--- server/services/db_service.test.ts
+++ server/services/db_service.test.ts
@@ -1,9 +1,19 @@
 import { RequestData } from "../types";
-import { addRequestToBasket } from "./db_service"; // Use import instead of require
+import { selectAllRequests, selectRequest, addRequestToBasket } from "./db_service"; // Use import instead of require
 
-console.log(1 + 2);
-  
-test('adds a request to the appropriate databases', async () => {   const myRequest: RequestData | null = await addRequestToBasket('1', new Date(), 'GET', 'some headers', 'some body');
-  console.log(myRequest);
+
+test('adds a request to the appropriate databases', async () => {
+  const myRequest: RequestData | null = await addRequestToBasket('1', new Date(), 'GET', 'some headers', 'some body');
   expect(myRequest?.headers).toBe('some headers');
-    });
+});
+
+test('gets body data from Mongo during select', async () => {
+  const myRequest: RequestData | null = await selectRequest('6');
+  console.log(myRequest);
+  expect(myRequest?.body).toBe('some body');
+});
+
+test('gets body data from Mongo during selectAll', async () => {
+  const myRequest: RequestData[] | null = await selectAllRequests('1');
+  expect(myRequest?.map(request => request.body)).toContain('some body');
+});
