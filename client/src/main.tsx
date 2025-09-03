312f49d4bc6dab50c21e11ef0de2219c8ad55f01
Benjamin Walker <benjamin.q.walker@gmail.com>
2025-09-02 19:18:15 -0500

    easter egg for Zane

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import faviconUrl from './assets/one_piece.png';
+
+const link = document.getElementById('favicon') as HTMLLinkElement | null;
+if (link) link.href = faviconUrl;
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
