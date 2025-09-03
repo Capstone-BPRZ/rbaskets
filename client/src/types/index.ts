296bae59d7591e6f7f187caa992882ac1bbc9889
Benjamin Walker <benjamin.q.walker@gmail.com>
2025-09-02 19:17:39 -0500

    updated node modules for axios
    # Conflicts:
    #	client/src/App.tsx

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-export interface Basket {
-  id: number
-=======
 export interface User {
   id: number,
 }
@@ -19,5 +15,4 @@ export interface Request {
   method: string,
   headers: string,
   body_id: string,
->>>>>>> b774eb38e314ec818dd8b04be5e11e597841b630
 }
